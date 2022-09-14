3b588af4b3b50d4645fe87ef231be831b7f15df2
k-landon <a1767709@student.adelaide.edu.au>
2022-09-14 17:39:55 +0930

    Draft priority_RR - I must say I do not know what I am doing

--- priority_RR.cpp
+++ priority_RR.cpp
@@ -1,11 +1,65 @@
 //
 // Created by kt on 9/09/22.
+//
+#include <iostream>
+#include <vector>
+#include <deque>
+
+using namespace std;
 
 
-Customers ** priority_queue(Customers ** list)
+bool first_response(Customer * customer)
 {
+    if(customer.playing_since <= 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
-    return false;
+
+bool small_time(Customer customer, int quantum)
+{
+    if(customer.slots_remaining <= quantum)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+ priority_queue(vector<Customer> &customers, int current, int quantum)
+{
+    vector<pair<int, Customer>> q;
+    for(int i = 0; i < customers.size(); i++)
+    {
+        if(customers[i].slots_remaining>=0)
+        {
+            int priority = 0;
+            if(first_response(customers[i]))
+            {
+                priority += 5;
+            }
+            if(small_time(customers[i], quantum))
+            {
+                priority += 4;
+            }
+            if(customers[i].priority == 1)
+            {
+                priority += 3;
+            }
+            q.push_back(make_pair(priority, customers[i]));
+
+        }
+    }
+    sort(q.begin(), q.end());
+    return q;
 }
 
 //
